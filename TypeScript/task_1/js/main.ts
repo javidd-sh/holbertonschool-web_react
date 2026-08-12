dcf2b099330096b3dcee910e3180d8371b8ad5ed
Your Name <you@example.com>
2026-08-12 19:07:55 +0000

    salam

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -16,3 +16,35 @@ const teacher3: Teacher = {
 };
 
 console.log(teacher3);
+export interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [propName: string]: any;
+}
+
+export interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
+export interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+export const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => {
+  return `${firstName.charAt(0)}. ${lastName}`;
+};
+
+// Example usage:
+console.log(printTeacher("John", "Doe")); // Prints: J. Doe
